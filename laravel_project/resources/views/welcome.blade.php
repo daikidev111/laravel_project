113a9286ffb06660720b65c2c981bf7ec8383ae2
daikidev111 <dkub0001@student.monash.edu>
2021-01-06 23:36:38 +0900

    login functionality has been implemented

--- laravel_project/resources/views/welcome.blade.php
+++ laravel_project/resources/views/welcome.blade.php
@@ -65,7 +65,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+		<div class="flex-center position-ref full-height">
+{{--
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -76,7 +77,7 @@
                     @endauth
                 </div>
             @endif
-
+--}}
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
