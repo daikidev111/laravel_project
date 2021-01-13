12f5e50769077994e2a6ef75456fefe7e272fd54
daikidev111 <dkub0001@student.monash.edu>
2021-01-14 04:23:02 +0900

    color modification

--- laravel_project/resources/views/layouts/app_admin.blade.php
+++ laravel_project/resources/views/layouts/app_admin.blade.php
@@ -14,7 +14,7 @@
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-blue.min.css">
 	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
 	<style> body{ background-color: tomato; } </style>
